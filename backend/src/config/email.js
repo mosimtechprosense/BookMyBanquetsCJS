7b99ad52b0c234601de7af406cbff3b98242bd19
Mosim Raza <mosimraza.techprosense@gmail.com>
2026-02-11 16:10:18 +0530

    fix email.js

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -1,15 +1,15 @@
 const nodemailer = require("nodemailer");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,         // TLS
-  secure: false,     // true only for port 465 (SSL)
+  host: "smtpout.secureserver.net",
+  port: 587,
+  secure: false, // 465 = true
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 module.exports = { transporter };
